87fb2ad471bcc64743bb8cfd53eafab63d2dcf99
jashDevkar <devkarjash1054@gmail.com>
2024-12-22 01:38:08 +0530

    finalized the app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:weatherly/screens/city_screen.dart';
 import 'package:weatherly/utils/constants.dart';
 import 'package:weatherly/services/networking.dart';
 
-
 Networking networking = Networking();
-
 
 class HomeScreen extends StatefulWidget {
   var data;
@@ -16,31 +16,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   late Map data;
   late int id;
   late String name;
-  late int seaLevel;
+  late num seaLevel;
+  late num groundLevel;
   late num windSpeed;
-  late int humidity;
-  late double temp;
-  late double minTemp;
-  late double maxTemp;
-  late double feelsLike;
+  late num humidity;
+  late num temp;
+  late num minTemp;
+  late num maxTemp;
+  late num feelsLike;
   late var networkIcon;
   late String description;
   late String country;
+  late double lat;
+  late double long;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     updateUi(widget.data);
-
   }
 
-  void updateUi(Map weatherData){
-    print(weatherData);
+  void updateUi(Map weatherData) {
     setState(() {
+      lat = weatherData['coord']['lat'];
+      long = weatherData['coord']['lon'];
+      groundLevel = weatherData['main']['grnd_level'];
       name = weatherData['name'];
       country = weatherData['sys']['country'];
       id = weatherData['weather'][0]['id'];
@@ -56,24 +59,25 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: true,
         backgroundColor: Colors.deepPurpleAccent,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           leading: IconButton(
-            onPressed: ()async {
-              try{
+            onPressed: () async {
+              try {
                 Map data = await networking.getLocationWeather();
                 updateUi(data);
-              }
-              catch(e){
-               ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$e',style: kErrorTextStyle,)));
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                  '$e',
+                  style: kErrorTextStyle,
+                )));
               }
             },
             icon: const Icon(
@@ -84,18 +88,21 @@ class _HomeScreenState extends State<HomeScreen> {
           actions: [
             IconButton(
                 onPressed: () async {
-                  try{
+                  try {
                     FocusScope.of(context).unfocus();
-                    String? cityName = await Navigator.push(context, MaterialPageRoute(builder: (context) => CityScreen()));
-                    if(cityName != null){
+                    String? cityName = await Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => CityScreen()));
+                    if (cityName != null) {
                       Map data = await networking.getCityWeather(cityName);
                       updateUi(data);
                     }
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                      '$e',
+                      style: kErrorTextStyle,
+                    )));
                   }
-                  catch(e){
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$e',style: kErrorTextStyle,)));
-                  }
-
                 },
                 icon: const Icon(
                   Icons.location_city,
@@ -105,88 +112,170 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Flexible(
-              flex: 9,
-              child: Center(
-                child:Container(
-                  width: 200.0,
-                  child: Image(image: networking.getImage(id),),
-                )
-              ),
-            ),
+            ///image
             Flexible(
               flex: 1,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.black87,
-                          borderRadius: BorderRadius.circular(10.0)
-                      ),
-                      margin: const EdgeInsets.only(bottom: 10.0,left: 10.0,right: 10.0),
-                      padding: const EdgeInsets.only(left: 10.0,right: 10.0,top: 10.0,bottom: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Weather :-  $description',style: kDetailTextStyle,),
-                        ],
-                      ),
-                    ),
+              child: SizedBox(
+                child: Center(
+                  child: Image(
+                    image: networking.getNetworkImage(networkIcon),
                   ),
-
-                ],
+                ),
               ),
             ),
+
+            ///details
             Flexible(
-              flex: 5,
-              child: Row(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Expanded(
+                  ///descripiton
+                  Flexible(
+                    flex: 1,
                     child: Container(
+                      margin: const EdgeInsets.all(10.0),
                       decoration: BoxDecoration(
                           color: Colors.black87,
-                          borderRadius: BorderRadius.circular(10.0)
-                      ),
-                      margin: const EdgeInsets.only(bottom: 30.0,left: 10.0),
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text('City name :  $name',style: kDetailTextStyle,),
-                          Text('Country code :  $country',style: kDetailTextStyle,),
-                          Text('Humidity :  $humidity%',style: kDetailTextStyle,),
-                          Text('Wind speed :  $windSpeed',style: kDetailTextStyle,),
-
-
-                        ],
-                      ),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Center(
+                          child: Text(
+                        'Weather: $description',
+                        style: kDetailTextStyle,
+                      )),
                     ),
                   ),
-                  const SizedBox(width: 20.0,),
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.black87,
-                          borderRadius: BorderRadius.circular(10.0)
-                      ),
-                      margin: const EdgeInsets.only(bottom: 30.0,right: 10.0),
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text('Temprature :  $temp',style: kDetailTextStyle,),
-                          Text('Feels like :  $feelsLike',style: kDetailTextStyle,),
-                          Text('Max temp :  $temp',style: kDetailTextStyle,),
-                          Text('Min temp :  $minTemp',style: kDetailTextStyle,),
 
+                  ///temprature and all
+                  Flexible(
+                      flex: 5,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.black87,
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              margin:
+                                  const EdgeInsets.only(left: 10.0, right: 5.0),
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'City name: $name',
+                                    style: kDetailTextStyle,
+                                  ),
+                                  Text(
+                                    'Country code: $country',
+                                    style: kDetailTextStyle,
+                                  ),
+                                  Text(
+                                    'Humidity: $humidity %',
+                                    style: kDetailTextStyle,
+                                  ),
+                                  Text(
+                                    'Wind Speed: $windSpeed m/s',
+                                    style: kDetailTextStyle,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(
+                                left: 5.0,
+                                right: 10.0,
+                              ),
+                              padding: const EdgeInsets.only(
+                                  left: 10.0, right: 10.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.black87,
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    'Temprature: $temp ℃',
+                                    style: kDetailTextStyle,
+                                  ),
+                                  Text(
+                                    'Feels Like: $feelsLike ℃',
+                                    style: kDetailTextStyle,
+                                  ),
+                                  Text(
+                                    'Min Temp: $minTemp ℃',
+                                    style: kDetailTextStyle,
+                                  ),
+                                  Text(
+                                    'Max Temp: $maxTemp ℃',
+                                    style: kDetailTextStyle,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         ],
-                      ),
-                    ),
-                  ),
+                      )),
+
+                  ///other details
+                  Flexible(
+                      flex: 2,
+                      child: Container(
+                        margin: const EdgeInsets.only(
+                            left: 10.0, right: 10.0, bottom: 10.0, top: 10.0),
+                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                        decoration: BoxDecoration(
+                            color: Colors.black87,
+                            borderRadius: BorderRadius.circular(10.0)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Flexible(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    'Latitide: $lat',
+                                    style: kDetailTextStyle,
+                                  ),
+                                  Text(
+                                    'Sea level: $seaLevel m',
+                                    style: kDetailTextStyle,
+                                  )
+                                ],
+                              ),
+                            ),
+                            Flexible(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    'Longitude: $long',
+                                    style: kDetailTextStyle,
+                                  ),
+                                  Text(
+                                    'Ground level: $groundLevel m',
+                                    style: kDetailTextStyle,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ))
                 ],
               ),
             ),
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -38,27 +38,8 @@ class Networking{
     }
   }
 
-  AssetImage getImage(var condition){
-    if (condition < 300) {
-      return const AssetImage('assets/images/thunderStorm.png');
-    } else if (condition < 400) {
-      return const AssetImage('assets/images/drizzle.png');
-    } else if (condition < 600) {
-      return const AssetImage('assets/images/rain.png');
-    } else if (condition < 700) {
-      return const AssetImage('assets/images/snow.png');
-    } else if (condition < 800) {
-      return const AssetImage('assets/images/atmosphere.png');
-    } else if (condition == 800) {
-      return const AssetImage('assets/images/clear-sky.png');
-    } else if (condition <= 804) {
-      return const AssetImage('assets/images/clouds.png');
-    } else {
-      return const AssetImage('assets/images/clear-sky.png');
-    }
-  }
 
   NetworkImage getNetworkImage(icon){
-    return NetworkImage('https://openweathermap.org/img/wn/$icon@2x.png',scale: 0.3);
+    return NetworkImage('https://openweathermap.org/img/wn/$icon@2x.png',scale: 0.7);
   }
 }
